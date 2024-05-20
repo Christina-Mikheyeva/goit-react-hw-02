d8e53d34ddde33373988cac6ff52349cf8e63c70
Christina Mikheyeva <c.mikheyeva@gmail.com>
2024-05-20 11:14:33 +0200

    Add buttons try map

--- src/App.jsx
+++ src/App.jsx
@@ -12,35 +12,29 @@ import Feedback from "./components/Feedback/Feedback";
 import Options from "./components/Options/Options";
 
 const App = () => {
-  const [state, setState] = useState({
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
   });
 
   function updateFeedback(feedbackType) {
-    setState({ ...state, feedbackType: state.feedbackType + 1 });
+    setFeedback({ ...feedback, [feedbackType]: feedback[feedbackType] + 1 });
   }
-
-  // const handeleClickG = () => {
-  //   setState({ ...state, good: state.good + 1 });
-  // };
-
-  // const handeleClickN = () => {
-  //   setState({ ...state, neutral: state.neutral + 1 });
-  // };
-
-  // const handeleClickB = () => {
-  //   setState({ ...state, bad: state.bad + 1 });
-  // };
-
+  const state = ["Good", "Bad", "Natural"];
   return (
     <>
       <Description />
-      <Options onClick={() => updateFeedback("good")}>Good</Options>
-      <Options onClick={() => updateFeedback("neutral")}>Neutral</Options>
-      <Options onClick={() => updateFeedback("bad")}>Bad</Options>
-      <Feedback state={state} />
+      {state.map((el) => (
+        <Options key={el} onClick={() => updateFeedback("good")}>
+          {el}
+        </Options>
+      ))}
+      <Feedback
+        good={feedback.good}
+        bad={feedback.bad}
+        natural={feedback.neutral}
+      />
     </>
   );
 };
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,12 +1,12 @@
 import css from "./Feddback.module.css";
 
-const Feedback = (state) => {
+const Feedback = ({ good, bad, natural }) => {
   return (
     <div>
       <ul className={css.listFeedback}>
-        <li className={css.feedback}>Good: {state.good}</li>
-        <li className={css.feedback}>Natural: {state.natural}</li>
-        <li className={css.feedback}>Bad: {state.bad}</li>
+        <li className={css.feedback}>Good: {good}</li>
+        <li className={css.feedback}>Natural: {natural}</li>
+        <li className={css.feedback}>Bad: {bad}</li>
         {/* <li className={css.feedback}>Total: {}</li>
         <li className={css.feedback}>Positive: {}</li> */}
       </ul>
